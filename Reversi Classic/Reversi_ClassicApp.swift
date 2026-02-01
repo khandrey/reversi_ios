334dcffbb595d851a440a9e3e28500188ec7a062
Andrei Khisamov <andrey@d-science.kz>
2026-02-01 15:08:48 +0300

    Initial Commit

---
+++ Reversi Classic/Reversi_ClassicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reversi_ClassicApp.swift
+//  Reversi Classic
+//
+//  Created by Andrey hisamov on 01.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Reversi_ClassicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
